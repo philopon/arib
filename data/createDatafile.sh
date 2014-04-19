a14d55707c617094206c0dd0375a4e964e2a2ad0
philopon <philopon.dependence@gmail.com>
2014-04-20 02:25:48 +0900

    add charset data.

--- data/createDatafile.sh
+++ data/createDatafile.sh
@@ -2,9 +2,10 @@
 
 cd `dirname $0`
 
-awk '/^3-/ && $2 != "#" {print substr($1,3,length($1)), substr($2,3,length($2))}' jisx0213-2004-std.txt \
-  | sed 's/+/ /g' > jisx0213-2004-std-1.txt
+awk '$1 != "#" {print substr($2, 3) "\t" substr($3, 3)}' JIS0208.TXT > Kanji.txt
+awk '/^3-/ && $2 != "#" {print substr($1,3) "\t" substr($2,3)}' jisx0213-2004-std.txt \
+  | sed 's/+/	/g' > jisKanji-1.txt
 
 
-awk '/^4-/ && $2 != "#" {print substr($1,3,length($1)), substr($2,3,length($2))}' jisx0213-2004-std.txt \
-  | sed 's/+/ /g' > jisx0213-2004-std-2.txt
+awk '/^4-/ && $2 != "#" {print substr($1,3) "\t" substr($2,3)}' jisx0213-2004-std.txt \
+  | sed 's/+/	/g' > jisKanji-2.txt
