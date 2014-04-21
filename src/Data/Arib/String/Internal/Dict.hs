903cecc19f62c0dea03f15d7660e82562df6d890
philopon <philopon.dependence@gmail.com>
2014-04-22 02:22:08 +0900

    fix index mistake.

--- src/Data/Arib/String/Internal/Dict.hs
+++ src/Data/Arib/String/Internal/Dict.hs
@@ -6,7 +6,7 @@ import qualified Data.ByteString as S
 pickDict :: Dict -> Int -> S.ByteString
 pickDict (Dict elen b) i =
     let (u,l) = i `quotRem` 0x100
-        ix    = 0x100 * (u - 0x21) + (l - 0x21)
+        ix    = 94 * (u - 0x21) + (l - 0x21)
         len   = fromIntegral $ b `S.index` (fromIntegral $ ix * elen) :: Int
     in S.take (fromIntegral len) $ S.drop (fromIntegral $ ix * elen + 1) b
 {-# INLINE pickDict #-}
