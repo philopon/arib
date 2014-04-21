ec2959ccd0a973a993131110637cd139ac35c261
philopon <philopon.dependence@gmail.com>
2014-04-22 02:26:14 +0900

    merge Dict and Charset.

--- src/Data/Arib/String/Internal/Charset.hs
+++ src/Data/Arib/String/Internal/Charset.hs
@@ -3,11 +3,17 @@
 
 module Data.Arib.String.Internal.Charset where
 
-import qualified Data.Text.Lazy.Builder as B
-import Data.Primitive.Array
-import System.IO.Unsafe
+import qualified Data.ByteString as S
 import Data.Arib.String.Internal.TH
 
 kanji :: Dict
 kanji = $(mkCharsetFunction "/Users/philopon/mysrc/Haskell/arib/data/Kanji.txt")
+
+pickDict :: Dict -> Int -> S.ByteString
+pickDict (Dict elen b) i =
+    let (u,l) = i `quotRem` 0x100
+        ix    = 94 * (u - 0x21) + (l - 0x21)
+        len   = fromIntegral $ b `S.index` (fromIntegral $ ix * elen) :: Int
+    in S.take (fromIntegral len) $ S.drop (fromIntegral $ ix * elen + 1) b
+{-# INLINE pickDict #-}
 
