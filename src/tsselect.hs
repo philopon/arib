125a7c6b86834797991d1f114b29d7eff710c248
philopon <philopon.dependence@gmail.com>
2014-04-29 16:40:40 +0900

    add programid select.

--- src/tsselect.hs
+++ src/tsselect.hs
@@ -3,16 +3,23 @@
 {-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE OverloadedStrings #-}
 
-import Data.Conduit
+import Control.Monad
 import Control.Monad.Trans
 import Control.Monad.Trans.Resource
-import qualified Data.Conduit.List as CL
+
 import System.Environment
+import System.Exit
+import System.Directory
+import System.IO
+
+import Data.Conduit
+import qualified Data.Conduit.List as CL
 import qualified Data.IntMap.Strict as IM
 import Data.Monoid
 import Data.Word
 import qualified Data.Text.Lazy as L
 import qualified Data.Text.Lazy.IO as L
+
 import Numeric
 
 import Data.Arib
@@ -37,7 +44,7 @@ instance Monoid Count where
         let d = if ac `isNextOf` bc then 0 else 1
             in Count (at + bt) (ad + bd + d) (ae + be) (as + bs) bo ac
 
-tsToCount :: TS a -> Int -> Count
+tsToCount :: TS -> Int -> Count
 tsToCount ts o = 
     Count { total     = 1
           , drops     = 0
@@ -47,8 +54,8 @@ tsToCount ts o =
           , cc        = continuityCounter ts
           }
 
-abstruct :: (IM.IntMap Count, Int) -> TS a -> (IM.IntMap Count, Int)
-abstruct (!m,!c) ts = (IM.insertWith mappend (tsProgramId ts) (tsToCount ts c) m, succ c)
+abstruct :: (IM.IntMap Count, Int) -> TS -> (IM.IntMap Count, Int)
+abstruct (!m,!c) ts =  {-# SCC "abstruct" #-}(IM.insertWith mappend (tsProgramId ts) (tsToCount ts c) m, succ c)
 {-# INLINE abstruct #-}
 
 showResult :: [(Int, Count)] -> [L.Text]
@@ -64,8 +71,22 @@ showResult l = map (\(pid,c) ->
     maxDigit   f = (+1) . floor . logBase 10 $ (fromIntegral . maximum $ map (f . snd) l :: Double)
     showElem c f = L.justifyRight (maxDigit f) ' ' (L.pack . show $ f c)
 
+select :: Monad m => Bool -> [Int] -> Conduit TS m TS
+select include pids = CL.filter (elm pids . tsProgramId)
+  where
+    elm = flip $ if include then elem else notElem
+
 main :: IO ()
 main = getArgs >>= \case
     [file] -> runResourceT $ do
         r <- fmap fst $ sourceTs file $$ CL.fold abstruct (IM.empty, 0)
         liftIO $ mapM_ L.putStrLn . showResult $ IM.toAscList r
+    src:dst:pidss_ -> do
+        let (include, pidss) = span (== "-x") pidss_ 
+        ex <- doesFileExist dst
+        when ex $ hPutStrLn stderr "dst file already exists." >> exitFailure
+        pids <- mapM readIO pidss :: IO [Int]
+        runResourceT $ sourceTs src $$ select (null include) pids =$ sinkTs dst
+    _ -> do
+        p <- getProgName
+        putStrLn $ "USAGE: " ++ p ++ " SRC [DST [-x] pid1 [pid2 ...]]"
